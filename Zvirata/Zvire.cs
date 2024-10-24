1a002dd7e03ea15db553e41db620ef0ac7f9b6e7
Filip Vavrečka <98539@vitkovickastredni.cz>
2024-10-24 08:59:33 +0200

    zvirata 2

--- Zvirata/Zvire.cs
+++ Zvirata/Zvire.cs
@@ -1,12 +1,17 @@
 using System.Security.Cryptography.X509Certificates;
 
-namespace zvirata
-{
-    class Zvire
-    {
-        static void Main(string[] args)
-        {
-            public Zvire(,  )
+namespace zvirata{
+    class Zvire{
+        
+            string rasa;
+            string zvuk;
+            int vek;
+
+            public Zvire(string rasa, string zvuk, int vek){
+                this.rasa = rasa;
+                this.zvuk = zvuk;
+                this.vek = vek;         
         }
+        
     }
 }
