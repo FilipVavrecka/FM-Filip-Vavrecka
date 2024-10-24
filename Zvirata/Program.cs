2e6889b9bd682cd4a779d9dac8b8098c64cafa27
Filip Vavrečka <98539@vitkovickastredni.cz>
2024-10-24 08:44:22 +0200

    zvirata-1

---
+++ Zvirata/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace zvirata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Zvirata/Zvire.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace zvirata
+{
+    class Zvire
+    {
+        static void Main(string[] args)
+        {
+            public Zvire(,  )
+        }
+    }
+}
