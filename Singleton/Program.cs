3dd02a22319c2486f0411739715cd9b78aaa25cb
Filip Vavrečka <98539@vitkovickastredni.cz>
2024-11-12 10:28:48 +0100

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+        }
+    }
+}
